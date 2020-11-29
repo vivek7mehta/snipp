0095d77289530f098154916a74cd1c2e6e451b11
Vivek Mehta <vivek7mehta@gmail.com>
2020-11-29 20:43:43 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small repo which will include small reusable utility functions required during ML projects.',
+    author='Vivek Mehta',
+    license='MIT',
+)
